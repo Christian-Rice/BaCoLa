cdd2cfaa1db84c519c6ae0e91af50f99777fad94
WhatWhoWhy <105808883+Christian-Rice@users.noreply.github.com>
2023-02-13 22:10:43 +0000

    Updated "update.sh" for debug.

--- update.sh
+++ update.sh
@@ -1,25 +1,42 @@
+i=0
 rm -rf "index.js" &&
 rm -rf "language" &&
 rm -rf "modules" &&
 
 clear &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/index.js")" > "index.js" &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/language")" > "language" &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/update.sh")" > "update.sh" &&
 
 mkdir "modules" &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/index.js")" > "modules/arrays.bclm" &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/modules/exampleModule.bclm")" > "modules/exampleModules.bclm" &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/modules/fs.bclm")" > "modules/fs.bclm" &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/modules/safeMode.bclm")" > "modules/safeMode.bclm" &&
 
+i=$((i+1))
+echo $i
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/bacola")" > "bacola" &&
 
 chmod +x * &&
