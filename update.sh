e57fa99f14f67bb767f0967c59ddaa0af9c1eb2c
WhatWhoWhy <105808883+Christian-Rice@users.noreply.github.com>
2023-02-13 22:26:07 +0000

    updated "update.sh"

--- update.sh
+++ update.sh
@@ -21,8 +21,12 @@ echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/mast
 
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/bacola")" > "bacola" &&
 
-if ! test -f "File2.txt" ; then
+if ! test -f "variables.json" ; then
   echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/variables.json")" > "variables.json";
+fi &&
+
+if ! test -f "index.bacola" ; then
+  echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/index.bacola")" > "index.bacola";
 fi &&
 
 echo "$(curl -fsSL "https://raw.githubusercontent.com/Christian-Rice/BaCoLa/master/variablesdefault.json")" > "variablesdefault.json" &&
